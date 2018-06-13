245adadfab8edca85ec6e5aafe4d17c84e184339
赵虎 <18726872353@163.com>
2018-06-13 08:57:53 +0800

    修改了index.js和index.html合并提交1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Li = document.getElementsByTagName('li');
 for (var i = 0; i < Li.length; i++) {
+    Li[i].index = i;
     Li[i].onclick = function () {
-        
+        console.log(200);
+        console.log(Li[i].index);
     }
 }
 
