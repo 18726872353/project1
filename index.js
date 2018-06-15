6a55acc25bc3484500c9efad3880fccef7dc3a49
赵虎 <18726872353@163.com>
2018-06-15 14:27:03 +0800

    完整选项卡1

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var op = document.getElementById("options");
 var oLi = op.getElementsByTagName('li');
 var oL = document.getElementById("list");
 var oLLi = oL.getElementsByTagName('li');
-for (var i = 0; i <oLi.length; i++) {
-    oLi[i].index = i;
+for (var i = 0; i < oLi.length; i++) {
+        oLi[i].index = i;
     oLi[i].onclick = function () {
         for (var j = 0; j < oLi.length; j++) {
             oLi[j].className = "";
         }
-        oLi[this.index].className = "cative";
+        oLi[this.index].className = "active";
         for (var i = 0; i < oLLi.length; i++) {
-            oLLi[i].style.display = "none";
+            oLLi[i].className = "";
         }
-        oLLi[this.index].style.dipslay = "block";
+        oLLi[this.index].className = "active1";
     }
 }
 
