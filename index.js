b89d96df81a086d651b169baf1798d67ab868dd1
赵虎 <18726872353@163.com>
2018-06-15 08:40:03 +0800

    在分支2上修改了index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var Li = document.getElementsByTagName('li');
+var oBox = document.getElementsByClassName("box");
 for (var i = 0; i < Li.length; i++) {
     Li[i].index = i;
     Li[i].onclick = function () {
-       for(var j=0;j<Li.length;j++){
-        Li[j].className="";
-       }
-       Li[this.index].className="cative";
+        for (var j = 0; j < Li.length; j++) {
+            Li[j].className = "";
+        }
+        Li[this.index].className = "cative";
+        for (var i = 0; i < oBox.length; i++) {
+            oBox[i].style.display = "none";
+        }
+        oBox[this.index].style.dipslay = "block";
     }
 }
 
